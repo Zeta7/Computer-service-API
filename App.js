5acdd883657229f0ff020dab9f2aeff99e4aaf93
Zeta7 <edson.joan.m.o97@gmail.com>
2022-05-25 17:00:40 -0500

    heroku

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ const express = require('express');
 const { usersRoutes } = require('./routes/UsersRoutes');
 const { repairsRoutes } = require('./routes/RepairsRoutes');
 const { globalErrorHandler } = require('./controllers/errorController');
+const helmet = require('helmet');
+const compression = require('compression');
+const morgan = require('morgan');
 
 const cors = require('cors');
 const App = express();
@@ -9,6 +12,14 @@ const App = express();
 App.use(cors());
 
 App.use(express.json());
+
+App.use(helmet());
+
+App.use(compression());
+//log incoming request
+//morgan ('dev')    ('combined')
+if (process.env.NODE_ENV === 'development') App.use(morgan('dev'));
+else App.use(morgan('combined'));
 
 App.use('/api/v1/users', usersRoutes);
 App.use('/api/v1/repairs', repairsRoutes);
